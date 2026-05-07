f3e9ea2f31de2b2e65e73b897abc57aba5baaa11
PrajjwalVajpayee <122729018+PrajjwalVajpayee@users.noreply.github.com>
2026-05-08 01:58:58 +0530

    Changes

--- Backend/src/prisma/client.js
+++
@@ -1,8 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const { env } = require('../config/env');
-
-const prisma = new PrismaClient({
-  log: env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
-});
-
-module.exports = { prisma };
--- Backend/src/utils/password.js
+++
@@ -1,6 +0,0 @@
-const bcrypt = require('bcryptjs');
-
-const hashPassword = (plain) => bcrypt.hash(plain, 12);
-const comparePassword = (plain, hashed) => bcrypt.compare(plain, hashed);
-
-module.exports = { hashPassword, comparePassword };
